98cd21dd14017cb4882b8d57b86d0826211aaa28
EmilVorre <evorre23@student.aau.dk>
2024-10-28 10:03:00 +0100

    added Spring Boot

---
+++ src/main/java/project/_semester/hasseris/Tutor_Administration_Platform/TutorAdministrationPlatformApplication.java
@@ -0,0 +1,13 @@
+package project._semester.hasseris.Tutor_Administration_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorAdministrationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorAdministrationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/_semester/hasseris/Tutor_Administration_Platform/TutorAdministrationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package project._semester.hasseris.Tutor_Administration_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorAdministrationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
