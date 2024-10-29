59ff84f248bef3fa16771dbb7e8ffe3626c56c06
EmilVorre <evorre23@student.aau.dk>
2024-10-29 10:57:45 +0100

    fixed the backup script

--- database/backup.sh
+++ database/backup.sh
@@ -4,6 +4,7 @@
 TIMESTAMP=$(date +"%F_%H-%M-%S")
 
 # Run mysqldump and save the backup file
-mysqldump -h mysql -u user -ppassword students > /backup/db_backup_$TIMESTAMP.sql
+mysqldump -h mysql-database -u user -ppassword students > /backup/db_backup_$TIMESTAMP.sql
+
 
 echo "Backup completed at $TIMESTAMP"
