5d45d406ec47093a401826e04a97ca908a4c5848
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-11-13 12:08:41 +0100

    List initialization and database class

---
+++ backend/src/main/java/project/backend/Database.java
@@ -0,0 +1,11 @@
+package project.backend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Database {
+    
+}
--- backend/src/main/java/project/backend/Tutee.java
+++ backend/src/main/java/project/backend/Tutee.java
@@ -1,8 +1,9 @@
 package project.backend;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Tutee extends Role {
-    List<Post> posts;
-    List<Collaboration> collaborations;
+    List<Post> posts = new LinkedList<>();
+    List<Collaboration> collaborations = new LinkedList<>();
 }
