6334f75203193da6ab503ee901bd95ff29a2d15c
Patrick <patrick@reiffenste.in>
2024-11-15 11:45:59 +0100

    Made HeadText depend on the path of the current route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,20 @@ import { Routes, Route } from "react-router-dom"
 import Homepage from './components/Homepage'
 import { Box, Typography } from '@mui/material';
 import HeadText from './components/HeadText';
+import { useLocation } from 'react-router-dom';
+
+// IMPORTANT: Remember to add to this when adding a new page route
+const locationNameMap = new Map<string, string>([
+  ["/", "Homepage"],
+  ["/test/anotherTest", "test"]
+]);
+
 
 export default function App() {
   return (
     <Box sx={{ height: "100vh", width: "100vw", backgroundColor: "secondary.main" }}>
 
-      <HeadText DisplayText={'Homepage'} />
+      <HeadText DisplayText={getDisplayText()} />
 
       <Box sx={{ height: "90%", width: "100%" }}>
         <Routes>
@@ -17,4 +25,22 @@ export default function App() {
       </Box>
     </Box>
   );
+}
+
+function getDisplayText(): string {
+  const location = useLocation().pathname;
+  const text = locationNameMap.get(location);
+  const regex = new RegExp("(?:.*[\/])(.+)$")
+  if (text != undefined) {
+    return text;
+  }
+
+  // Handle path not found
+  const match = regex.exec(location);
+  if (match != null) {
+    return match[1]; //Use last capture group
+  }
+
+  // Regex couldn't fix
+  throw new Error("Regex could not handle pathname");
 }
