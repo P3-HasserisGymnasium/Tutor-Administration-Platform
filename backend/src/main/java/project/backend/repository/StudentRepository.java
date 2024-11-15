f708d5c72f33a10e7c9cbad197df47ac5500d77d
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-11-15 11:04:44 +0100

    Fixed some errors

--- backend/src/main/java/project/backend/repository/StudentRepository.java
+++ backend/src/main/java/project/backend/repository/StudentRepository.java
@@ -1,5 +1,8 @@
 package project.backend.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import project.backend.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     // Custom queries can be added here
--- backend/src/main/java/project/backend/repository/UserRepository.java
+++ backend/src/main/java/project/backend/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package project.backend.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import project.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     // Custom queries for users
