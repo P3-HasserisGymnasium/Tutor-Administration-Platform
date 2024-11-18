d2219761d7732b971cddbdfa858820ae50d66288
Lynneruup <76641014+Lynneruup@users.noreply.github.com>
2024-11-18 10:41:05 +0100

    Updated stuff

--- frontend/api/api-client.ts
+++ frontend/api/api-client.ts
@@ -1,61 +1,64 @@
-import axios from 'axios';
-import { QueryClient, MutationCache } from '@tanstack/react-query';
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import axios, { AxiosError } from "axios";
+import { QueryClient, MutationCache } from "@tanstack/react-query";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 // Create an Axios instance
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080',
-    withCredentials: true, // Ensure credentials (cookies) are sent with requests
+	baseURL: "http://localhost:8080",
+	withCredentials: true, // Ensure credentials (cookies) are sent with requests
 });
 
 // Intercept the response to handle token expiration
 apiClient.interceptors.response.use(
-    function (response) {
-        return response;
-    },
-    function (error) {
-        if (error?.response?.status === 401) {
-            // If the token is expired or invalid, log the user out
-            // Handle logout by removing cookies or redirecting to login page
-            // Example: window.location.href = "/login";
-        }
-        return Promise.reject(error);
-    }
+	function (response) {
+		return response;
+	},
+	function (error) {
+		if (error?.response?.status === 401) {
+			// If the token is expired or invalid, log the user out
+			// Handle logout by removing cookies or redirecting to login page
+			// Example: window.location.href = "/login";
+		}
+		return Promise.reject(error);
+	}
 );
 
 // Create a new query client
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            networkMode: 'offlineFirst', // Use cache first, else fetch
-            retry: (failureCount, error) => {
-                if (!Object.hasOwn(error, 'response')) {
-                    return false;
-                }
-                // Auth errors or 404 errors should not be retried
-                if ((error as any).response?.status === 401 || (error as any).response?.status === 404) {
-                    return false;
-                }
-                if (failureCount < 3) {
-                    return true;
-                }
-                return false;
-            },
-        },
-    },
-    mutationCache: new MutationCache({
-        onSuccess: () => {
-            queryClient.invalidateQueries(); // Invalidate all queries on success for refetch
-        },
-        onError: (error) => {
-            console.log('Error occurred: ', error);
-        },
-    }),
+	defaultOptions: {
+		queries: {
+			networkMode: "offlineFirst", // Use cache first, else fetch
+			retry: (failureCount, error) => {
+				if (!Object.prototype.hasOwnProperty.call(error, "response")) {
+					return false;
+				}
+				// Auth errors or 404 errors should not be retried
+				if (
+					(error as AxiosError).response?.status === 401 ||
+					(error as AxiosError).response?.status === 404
+				) {
+					return false;
+				}
+				if (failureCount < 3) {
+					return true;
+				}
+				return false;
+			},
+		},
+	},
+	mutationCache: new MutationCache({
+		onSuccess: () => {
+			queryClient.invalidateQueries(); // Invalidate all queries on success for refetch
+		},
+		onError: (error) => {
+			console.log("Error occurred: ", error);
+		},
+	}),
 });
 
 // Create a new persister
 const persister = createSyncStoragePersister({
-    storage: localStorage,
+	storage: localStorage,
 });
 
 export { apiClient, queryClient, persister };
