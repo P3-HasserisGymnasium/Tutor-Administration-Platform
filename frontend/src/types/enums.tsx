d0c240da0cada8580d514f72a92b6937675560c4
Kasia-eto <143899969+Kasia-eto@users.noreply.github.com>
2024-11-21 14:11:45 +0100

    created enums file

---
+++ frontend/src/types/enums.tsx
@@ -0,0 +1,20 @@
+// Enum for subjects
+export const Subjects = {
+    MUSIC: "Music",
+    MATH: "Math",
+    ESS: "EScience",
+    ENGLISH: "English",
+    DANISH: "Danish",
+    TYSK: "Tysk",
+    FRENCH: "French",
+    BIOLOGY:"Biology",
+    CHEMISTRY:"Chemistry",
+    
+};
+
+// Enum for subject colors
+export const SubjectColor = {
+    [Subjects.MUSIC]: {backgroundColor:"#b2c9e0", color: "white"},
+    [Subjects.MATH]: {backgroundColor: "#ffcccb", color: "white"},
+    [],
+}
