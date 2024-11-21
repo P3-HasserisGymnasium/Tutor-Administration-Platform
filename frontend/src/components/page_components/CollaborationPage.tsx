fde49aca71d10ff7c83f422174b1eba6d8cc31f7
Patrick <patrick@reiffenste.in>
2024-11-21 13:19:27 +0100

    added the last page components and their routes I believe

---
+++ frontend/src/components/page_components/CollaborationPage.tsx
@@ -0,0 +1,11 @@
+import MediumShortOnShortBoxLayout from "components/layout_components/MediumShortOnShortBoxLayout"
+
+export default function CollaborationPage() {
+	return (
+		<MediumShortOnShortBoxLayout>
+			<div>Put scedule here</div>
+			<div>Put tutor communication here</div>
+			<div>Put meetings here</div>
+		</MediumShortOnShortBoxLayout>
+	);
+};
