53fdc6d3d44970b7a575796ea5c06f55fcb20d21
Lynneruup <76641014+Lynneruup@users.noreply.github.com>
2024-11-26 06:59:34 +0100

    Added Subject Enum to PostType

--- frontend/src/types/entity_types.ts
+++ frontend/src/types/entity_types.ts
@@ -1,59 +1,65 @@
-import { z } from 'zod'
-import { Language, Subject, YearGroup, TimeSlot, MeetingState, CollaborationState, zodUUID } from './data_types'
+import { z } from "zod";
+import {
+	Language,
+	Subject,
+	YearGroup,
+	TimeSlot,
+	MeetingState,
+	CollaborationState,
+	zodUUID,
+} from "./data_types";
 
 export const zodPostSchema = z.object({
-    id: zodUUID,
-    title: z.string(),
-    description: z.string(),
-    subject: z.string(),
-    duration: z.string(),
-    state: z.string(),
-
+	id: zodUUID,
+	title: z.string(),
+	description: z.string(),
+	subject: Subject,
+	duration: z.string(),
+	state: z.string(),
 });
 
 export const zodProfileSchema = z.object({
-    full_name: z.string(),
-    year_group: YearGroup,
-    languages: z.array(Language),
-    subjects: z.array(Subject),
-    description: z.string().optional(),
+	full_name: z.string(),
+	year_group: YearGroup,
+	languages: z.array(Language),
+	subjects: z.array(Subject),
+	description: z.string().optional(),
 });
 
 export const zodMeetingSchema = z.object({
-    id: zodUUID,
-    rejection_reason: z.string().optional(),
-    end_date: z.date(),
-    collaboration_id: zodUUID,
-    meeting_description: z.string().optional(),
-    state: MeetingState,
-    start_date: z.date(),
+	id: zodUUID,
+	rejection_reason: z.string().optional(),
+	end_date: z.date(),
+	collaboration_id: zodUUID,
+	meeting_description: z.string().optional(),
+	state: MeetingState,
+	start_date: z.date(),
 });
 
 export const zodCollaborationSchema = z.object({
-    end_date: z.date().optional(),
-    tutee_id: zodUUID,
-    start_date: z.date().optional(),
-    tutor_id: zodUUID,
-    state: CollaborationState,
-    subject: Subject,
+	end_date: z.date().optional(),
+	tutee_id: zodUUID,
+	start_date: z.date().optional(),
+	tutor_id: zodUUID,
+	state: CollaborationState,
+	subject: Subject,
 });
 
 export const zodFeedbackSchema = z.object({
-    id: zodUUID,
-    feedback: z.string(),
+	id: zodUUID,
+	feedback: z.string(),
 });
 
 export const zodAccountRegisterSchema = z.object({
-    full_name: z.string(),
-    email: z.string(),
-    password: z.string(),
-    year_group: YearGroup.optional(),
-    languages: z.array(Language).optional(),
-    tutoring_subjects: z.array(Subject).optional(),
-    tutor_profile_description: z.string().optional(),
-    tutor_timeslots: z.array(TimeSlot).optional(),
+	full_name: z.string(),
+	email: z.string(),
+	password: z.string(),
+	year_group: YearGroup.optional(),
+	languages: z.array(Language).optional(),
+	tutoring_subjects: z.array(Subject).optional(),
+	tutor_profile_description: z.string().optional(),
+	tutor_timeslots: z.array(TimeSlot).optional(),
 });
-
 
 export type PostType = z.infer<typeof zodPostSchema>;
 export type ProfileType = z.infer<typeof zodProfileSchema>;
