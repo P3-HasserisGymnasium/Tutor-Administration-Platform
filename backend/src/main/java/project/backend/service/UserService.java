e5ee157a67e2f19f1a3055465b834f3acf4ebb0b
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-11-28 14:28:45 +0100

    Basic UserController set up

--- backend/src/main/java/project/backend/service/UserService.java
+++ backend/src/main/java/project/backend/service/UserService.java
@@ -1,8 +1,32 @@
 package project.backend.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import project.backend.model.User;
+import project.backend.repository.UserRepository;
 
 @Service
 public class UserService {
     
+    @Autowired
+    final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
 }
