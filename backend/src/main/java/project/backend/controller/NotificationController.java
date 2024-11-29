1e57343109f3ea2992862200ab665f6c9f49819a
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-11-29 14:35:21 +0100

    wow vær så god patrickatis

--- backend/src/main/java/project/backend/controller/NotificationController.java
+++ backend/src/main/java/project/backend/controller/NotificationController.java
@@ -1,5 +1,6 @@
 package project.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,7 @@ import project.backend.model.Notification;
 import project.backend.service.NotificationService;
 
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/notifications")
 public class NotificationController {
