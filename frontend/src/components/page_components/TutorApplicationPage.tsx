635aa03e074ff9229fce578baffeb9a45d7eef11
Lynneruup <andreaslynnerup@outlook.dk>
2024-12-02 17:06:22 +0100

    prep for merge

--- frontend/src/components/page_components/TutorApplicationPage.tsx
+++ frontend/src/components/page_components/TutorApplicationPage.tsx
@@ -1,12 +1,12 @@
 import {ThemeProvider} from "@mui/material"
 import LongShortBoxLayout from "components/layout_components/LongShortBoxLayout";
-import { getCurrentTheme } from "~/utilities/helperFunctions";
+import { useCurrentTheme } from "~/utilities/helperFunctions";
 import TutorApplication from "components/content_components/TutorApplicationComponents/TutorApplication";
 import InformationBox from "../content_components/TutorApplicationComponents/InformationBox";
 
 export default function TutorApplicationPage() {
     return (
-        <ThemeProvider theme={getCurrentTheme()}>
+        <ThemeProvider theme={useCurrentTheme()}>
             <LongShortBoxLayout>
                 <TutorApplication/>
                 <InformationBox/>
