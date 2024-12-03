3f13ee40b6d45d204925142583ec03f26f44c994
Lynneruup <andreaslynnerup@outlook.dk>
2024-12-03 14:03:11 +0100

    Is this really it ?

--- backend/src/main/java/project/backend/model/Administrator.java
+++ backend/src/main/java/project/backend/model/Administrator.java
@@ -5,5 +5,18 @@ import jakarta.persistence.Entity;
 @Entity
 public class Administrator extends User{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
     public Administrator() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
