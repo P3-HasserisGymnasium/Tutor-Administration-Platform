8f5649307d4de75113a695b3a899d63a8d86746d
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-12-03 14:33:26 +0100

    Update YearGroupEnum.java

--- backend/src/main/java/project/backend/model/YearGroupEnum.java
+++ backend/src/main/java/project/backend/model/YearGroupEnum.java
@@ -1,22 +1,7 @@
 package project.backend.model;
 
 public enum YearGroupEnum {
-    PRE_IB("PRE_IB"),
-    IB_1("IB_1"),
-    IB_2("IB_2");
-
-    private final String displayName;
-
-    YearGroupEnum(String diplayName) {
-        this.displayName = diplayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    @Override
-    public String toString() {
-        return displayName;
-    }
+    PRE_IB,
+    IB_1,
+    IB_2;
 }
