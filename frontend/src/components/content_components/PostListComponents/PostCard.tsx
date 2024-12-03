fd667fbaa664fdea6414cf43499c3d4818ae994a
Lukas Saltenis <tl00xu@student.aau.dk>
2024-12-03 13:21:43 +0100

    Done with post-list page components. Functionality missing

--- frontend/src/components/content_components/PostListComponents/PostCard.tsx
+++ frontend/src/components/content_components/PostListComponents/PostCard.tsx
@@ -1,9 +1,36 @@
+import { Box, Button } from "@mui/material";
+import { PostType } from "~/types/entity_types";
+import { useTheme } from "@mui/system";
+import { Theme } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+import SubjectChip from "../SubjectChip";
+import { SubjectType } from "~/types/data_types";
 
-export default function PostCard({ post }: { post: Post }) {
+export default function PostCard({post}: { post: PostType }) {
+  const theme = useTheme<Theme>();
   return (
-    <div className="post-card">
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
-    </div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          backgroundColor: theme.customColors.collaborationBackgroundColor,
+          border: "1px solid "+ theme.customColors.headingTextColor,
+          padding: "1em",
+          borderRadius: "0.5em",
+        }}
+      >
+        <Box sx={{display: "flex", flexDirection:"column", gap:"0.4em"}}>
+            <Typography variant="h3">{post.title}</Typography>
+            <Box sx={{display: "flex", flexDirection:"row", alignItems:"center"}}>
+              <Typography variant="h4">Subject:</Typography>
+              <SubjectChip Subject={post.subject as SubjectType}></SubjectChip>
+            </Box>
+            <Typography variant="h4">Duration: {post.duration} months</Typography>
+            <Button variant="contained" sx={{alignSelf: "flex-start"}}>Request Collaboration</Button>
+        </Box>
+        <Box sx={{display: "flex", flexDirection:"column", flexGrow: 1, paddingLeft:"1em"}}>
+            <Typography variant="body1">{post.description}</Typography>
+        </Box>
+      </Box>
   );
 }
--- frontend/src/components/page_components/tutor/PostsListPage.tsx
+++ frontend/src/components/page_components/tutor/PostsListPage.tsx
@@ -1,13 +1,15 @@
 import ShortLongBoxLayout from "components/layout_components/ShortLongBoxLayout";
 import { ThemeProvider } from "@mui/material";
 import tutorTheme from "~/themes/tutorTheme";
+import PostFilter from "~/components/content_components/PostListComponents/PostFilter";
+import PostList from "~/components/content_components/PostListComponents/PostList";
 
 export default function PostsListPage() {
     return (
         <ThemeProvider theme={tutorTheme}>
             <ShortLongBoxLayout>
-                <div>Put filters here</div>
-                <div>Put posts here</div>
+                <PostFilter/>
+                <PostList/>
             </ShortLongBoxLayout>
         </ThemeProvider>
     );
