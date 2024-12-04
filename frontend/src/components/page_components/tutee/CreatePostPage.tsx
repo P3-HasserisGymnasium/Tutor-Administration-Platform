3de5290e48774710a239329739ded929e1c47f5b
Patrick <patrick@reiffenste.in>
2024-12-04 14:05:39 +0100

    merge conflict fix

--- frontend/src/components/page_components/tutee/CreatePostPage.tsx
+++ frontend/src/components/page_components/tutee/CreatePostPage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ThemeProvider } from "@mui/material";
 import EvenBoxLayout from "~/components/layout_components/EvenBoxLayout";
 import PostCreation from "~/components/content_components/PostCreationComponents/PostCreation";
@@ -15,16 +14,3 @@ export default function CreatePostPage() {
 		</ThemeProvider>
 	);
 }
-=======
-import LongShortBoxLayout from "components/layout_components/LongShortBoxLayout";
-
-
-export default function CreatePostPage() {
-    return (
-            <LongShortBoxLayout>
-                <div>Put input fields and stuff for the post creation here</div>
-                <div>Put information here</div>
-            </LongShortBoxLayout>
-    );
-};
->>>>>>> origin/main
