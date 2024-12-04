8c8d05bfc5a7827502322fc5f261b694e4257a11
EmilVorre <evorre23@student.aau.dk>
2024-12-04 12:47:49 +0100

    fixed compilling of backend

--- backend/src/main/java/project/backend/controller/AccountController.java
+++ backend/src/main/java/project/backend/controller/AccountController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import project.backend.controller_bodies.AccountControllerBodies.AccountLoginBody;
-import project.backend.controller_bodies.AccountControllerBodies.AccountRegisterBody;
+import project.backend.controller_bodies.account_controller.AccountLoginBody;
+import project.backend.controller_bodies.account_controller.AccountRegisterBody;
 import project.backend.model.User;
 import project.backend.service.AccountService;
 
