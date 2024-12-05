2e2604d1abbac9e9c7ee997736db53419afe79d8
Patrick <patrick@reiffenste.in>
2024-12-05 11:20:42 +0100

    redirect when not logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import { ThemeProvider } from "@mui/material";
 import { useCurrentTheme } from "./utilities/helperFunctions";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "./components/login_and_register_components/Register";
 import Login from "./components/login_and_register_components/Login";
 
@@ -11,7 +11,6 @@ import { Suspense, lazy } from "react";
 import Loading from "./api/authentication/Loading";
 import unauthenticatedAppTheme from "~/themes/unauthenticatedAppTheme";
 import { useAuth } from "./api/authentication/useAuth";
-import NotFound from "./api/authentication/NotFound";
 
 const AuthenticatedApp = lazy(() => import("./AuthenticatedApp"));
 
@@ -19,13 +18,15 @@ export default function App() {
   const theme = useCurrentTheme(); // must use hook to make sure the theme is updated, stateful
   const { isAuthenticated } = useAuth();
   console.log("isAuthenticated", isAuthenticated);
+
+
   if (!isAuthenticated) {
     return (
       <ThemeProvider theme={unauthenticatedAppTheme}>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </ThemeProvider>
     );
