7c93c494eb8dc1d1e0a01c78f8760d37e3637bc5
Lukas Saltenis <tl00xu@student.aau.dk>
2024-12-05 13:51:33 +0100

    TutorProfile page added

---
+++ frontend/src/components/content_components/TutorProfile.tsx
@@ -0,0 +1,12 @@
+import {Box, Button }from "@mui/material";
+
+export default function TutorProfile() {
+    return (
+        <Box sx={{display:"flex", flexDirection:"row", justifyContent:"end"}}>
+            <Button sx={{}}>Edit profile</Button>
+            <Box sx={{display:"flex", flexDirection:"column"}}>
+                
+            </Box>
+        </Box>
+    );
+}
