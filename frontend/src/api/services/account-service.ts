cef655b78a65547f6b280661a0a5e3663b34e562
Lynneruup <76641014+Lynneruup@users.noreply.github.com>
2024-12-07 10:22:43 +0100

    Added error messages to account service, moved cookie functions to helper functions

--- frontend/src/api/services/account-service.ts
+++ frontend/src/api/services/account-service.ts
@@ -4,7 +4,8 @@ import { AxiosError } from "axios";
 import { apiClient } from "../api-client";
 import { AccountRegisterType, LoginSuccessDataType, LoginType } from "~/types/entity_types";
 import { AccountRegisterResponseType } from "~/types/data_types";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { deleteCookie } from "~/utilities/helperFunctions";
 
 export const useAccountService = () => {
 	const navigate = useNavigate();
@@ -13,63 +14,63 @@ export const useAccountService = () => {
 			mutationKey: ["registerAccount"],
 			mutationFn: async (account: AccountRegisterType) => {
 				console.log(account);
-				const { data } = await apiClient.post<AccountRegisterResponseType>(
-					"/api/account/",
-					account
-				);
+				const { data } = await apiClient.post<AccountRegisterResponseType>("/api/account/", account);
 				return data;
 			},
-			onError: (e: AxiosError<{ detail: string }>) => {
-				toast.error(e?.response?.data?.detail);
+			onError: (e: AxiosError) => {
+				toast.error("Creation of account failed: " + e?.response?.data);
 			},
 			onSuccess: (data) => {
 				navigate("/login");
 				if (data.tutee) {
-					toast.success("Your account has been created and you can log in as a tutee!")
+					toast.success("Your account has been created and you can log in as a tutee!");
 				}
 				if (data.tutor) {
-					toast.info("When an administrator accepts your tutor application, you will be granted access to the system as a tutor")
+					toast.info(
+						"When an administrator accepts your tutor application, you will be granted access to the system as a tutor"
+					);
 				}
 			},
 		});
-	}
+	};
 
 	const useRemoveAccount = () => {
 		return useMutation({
 			mutationKey: ["removeAccount"],
-			mutationFn: async (accountId: number) => {
-				await apiClient.delete(`/api/account/${accountId}`);
+			mutationFn: async (userId: string) => {
+				await apiClient.delete(`/api/account/${userId}`);
 			},
-			onError: (e: AxiosError<{ detail: string }>) => {
-				toast.error(e?.response?.data?.detail);
+			onError: (e: AxiosError) => {
+				toast.error("Failed to remove account: " + e?.response?.data);
 			},
 			onSuccess: () => {
 				toast.success("Bruger slettet");
+				deleteCookie("Bearer");
+				deleteCookie("user");
+				deleteCookie("isAuthenticated");
 			},
-		})
+		});
 	};
-
-
-
-
 
 	const useLogin = () => {
 		return useMutation({
 			mutationKey: ["login"],
 			mutationFn: async (login_information: LoginType) => {
-				const { data } = await apiClient.post<LoginSuccessDataType>(
-					"/api/account/login",
-					login_information
-				);
+				const { data } = await apiClient.post<LoginSuccessDataType>("/api/account/login", login_information);
 				return data;
 			},
-			onError: (e: AxiosError<{ detail: string }>) => {
-				toast.error("Login failed" + (e?.response?.data?.detail == undefined ? "" : (": " + e?.response?.data?.detail)));
+			onError: (e: AxiosError) => {
+				if (e.message === "Network Error") {
+					toast.error("Login failed: Network Error: connection to server not established");
+				} else {
+					console.log("error", e);
+					toast.error("Login failed" + (e?.response?.data ? ": " + e.response.data : ""));
+				}
 			},
 			onSuccess: () => {
 				toast.success("You have been logged in");
 			},
-		})
+		});
 	};
 
 	const useLogout = () => {
@@ -78,14 +79,14 @@ export const useAccountService = () => {
 			mutationFn: async () => {
 				await apiClient.post("/api/account/logout");
 			},
-			onError: (e: AxiosError<{ detail: string }>) => {
-				toast.error(e?.response?.data?.detail);
+			onError: (e: AxiosError) => {
+				toast.error("Logout failed: " + e?.response?.data);
 			},
 			onSuccess: () => {
 				toast.success("You have been logged out");
 			},
-		})
-	}
+		});
+	};
 
 	return {
 		useRegisterAccount,
