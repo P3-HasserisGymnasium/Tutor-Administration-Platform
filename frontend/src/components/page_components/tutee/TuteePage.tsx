af0aeebeed49e97bc57ebd0082e26751e2baf631
Lynneruup <76641014+Lynneruup@users.noreply.github.com>
2024-12-10 16:18:33 +0100

    dasd

--- frontend/src/components/page_components/tutee/TuteePage.tsx
+++ frontend/src/components/page_components/tutee/TuteePage.tsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { Box, Button, Typography, ButtonGroup, IconButton, Tooltip, ThemeProvider } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 // import tutorTheme from "~/themes/tutorTheme";
-import MiniCollabList from "~/components/content_components/MiniCollabList";
-import MiniPostList from "~/components/content_components/MiniPostList";
-import MiniCalendar from "~/components/content_components/MiniCalendar";
+import MiniCollabList from "src/components/content_components/MiniCollabList";
+import MiniPostList from "src/components/content_components/MiniPostList";
+import MiniCalendar from "src/components/content_components/MiniCalendar";
 import MeetingsList from "src/components/content_components/MeetingsList";
 //import { useNavigate } from "react-router-dom";
 import { useCurrentTheme, useBreakpoints } from "~/utilities/helperFunctions";
@@ -18,51 +18,14 @@ import EditPostDialog from "../dialogs/EditPostDialog";
 import { PostState, Subject } from "~/types/data_types";
 import { PostType } from "~/types/entity_types";
 const post: PostType = {
-  id: 1,
-  title: "hjælp mig",
-  description: "iosjdsiaojdaojdasdiasdjsaod jsadj iasojdasd",
-  subject: Subject.Enum.DANISH,
-  duration: [2, 8],
-  state: PostState.Enum.VISIBLE,
+	id: 1,
+	title: "hjælp mig",
+	description: "iosjdsiaojdaojdasdiasdjsaod jsadj iasojdasd",
+	subject: Subject.Enum.DANISH,
+	duration: [2, 8],
+	state: PostState.Enum.VISIBLE,
 };
 export default function TuteePage() {
-<<<<<<< HEAD
-  const theme = useCurrentTheme();
-  const { isMobile } = useBreakpoints();
-  const [view, setView] = useState<"list" | "calender">("list");
-  const [showCollabDialog, setShowCollabDialog] = useState(false);
-  const { userState } = useAuth();
-  const { data: posts, isLoading: postsLoading, isError: postsError } = usePostService().useGetTuteePosts();
-  const {
-    data: collaborations,
-    isLoading: collabLoading,
-    isError: collabError,
-  } = useCollaborationService().useGetCollaborationsWithTutee(userState?.id || null);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <ViewCollaborationsDialog
-        open={showCollabDialog}
-        setOpen={setShowCollabDialog}
-        collaborations={collaborations}
-        isLoading={collabLoading}
-      />
-      <MediumShortOnShortBoxLayout>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            border: "1px solid #white",
-            borderRadius: "8px",
-            overflow: "hidden",
-            p: isMobile ? 1 : 2,
-            position: "relative",
-          }}
-        >
-          {/* Header with Title and Buttons */}
-          <Box sx={{ display: "flex", alignItems: "center", gap: isMobile ? 2 : 17 }}>
-            <Typography variant="h6">{view === "calender" ? "Calendar" : "Meeting List"}</Typography>
-=======
 	const theme = useCurrentTheme();
 	const { isMobile } = useBreakpoints();
 	const [view, setView] = useState<"list" | "calender">("list");
@@ -84,7 +47,7 @@ export default function TuteePage() {
 				collaborations={collaborations}
 				isLoading={collabLoading}
 			/>
-			<EditPostDialog open={showEditPostDialog} setOpen={setShowEditPostDialog} post={post}/>
+			<EditPostDialog open={showEditPostDialog} setOpen={setShowEditPostDialog} post={post} />
 			<MediumShortOnShortBoxLayout>
 				<Box
 					sx={{
@@ -100,158 +63,162 @@ export default function TuteePage() {
 					{/* Header with Title and Buttons */}
 					<Box sx={{ display: "flex", alignItems: "center", gap: isMobile ? 2 : 17 }}>
 						<Typography variant="h6">{view === "calender" ? "Calendar" : "Meeting List"}</Typography>
->>>>>>> c52096ebdce62a603476aefed2fc5dee7bd7f664
 
-            {/* Button group for toggling between calendar and meeting */}
-            <ButtonGroup variant="outlined" aria-label="outlined primary button group">
-              <Button
-                sx={{
-                  color: theme.palette.text.primary, // Dynamically use the theme's text color
-                  backgroundColor: view === "list" ? theme.palette.primary.main : theme.palette.background.paper,
-                  "&:hover": {
-                    backgroundColor: view === "list" ? theme.palette.primary.light : theme.palette.action.hover,
-                  },
-                }}
-                onClick={() => setView("calender")}
-              >
-                Show Calendar
-              </Button>
+						{/* Button group for toggling between calendar and meeting */}
+						<ButtonGroup variant="outlined" aria-label="outlined primary button group">
+							<Button
+								sx={{
+									color: theme.palette.text.primary, // Dynamically use the theme's text color
+									backgroundColor: view === "list" ? theme.palette.primary.main : theme.palette.background.paper,
+									"&:hover": {
+										backgroundColor: view === "list" ? theme.palette.primary.light : theme.palette.action.hover,
+									},
+								}}
+								onClick={() => setView("calender")}
+							>
+								Show Calendar
+							</Button>
 
-              <Button
-                sx={{
-                  color: theme.palette.text.primary,
-                  backgroundColor: view === "calender" ? theme.palette.primary.main : theme.palette.background.paper,
-                  "&:hover": {
-                    backgroundColor: view === "calender" ? theme.palette.primary.light : theme.palette.action.hover,
-                  },
-                }}
-                onClick={() => setView("list")}
-              >
-                Show Meetings
-              </Button>
-            </ButtonGroup>
+							<Button
+								sx={{
+									color: theme.palette.text.primary,
+									backgroundColor: view === "calender" ? theme.palette.primary.main : theme.palette.background.paper,
+									"&:hover": {
+										backgroundColor: view === "calender" ? theme.palette.primary.light : theme.palette.action.hover,
+									},
+								}}
+								onClick={() => setView("list")}
+							>
+								Show Meetings
+							</Button>
+						</ButtonGroup>
 
-            <Tooltip
-              title="Your schedule showcases all of your meetings across all your collaborations, as a tutee. Click on a meeting to go to the specific collaboration."
-              arrow
-            >
-              <IconButton
-                sx={{
-                  position: "absolute",
-                  top: 8,
-                  right: 8,
-                  alignItems: "right",
-                }}
-                aria-label="info"
-              >
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-          </Box>
-          <Box
-            sx={{
-              position: "flex",
-              alignContent: "center",
-              alignItems: "center",
-              border: "white 1px",
-              overflow: "hidden",
-            }}
-          >
-            {view === "calender" ? <MiniCalendar /> : <MeetingsList />}
-          </Box>
-        </Box>
+						<Tooltip
+							title="Your schedule showcases all of your meetings across all your collaborations, as a tutee. Click on a meeting to go to the specific collaboration."
+							arrow
+						>
+							<IconButton
+								sx={{
+									position: "absolute",
+									top: 8,
+									right: 8,
+									alignItems: "right",
+								}}
+								aria-label="info"
+							>
+								<InfoIcon />
+							</IconButton>
+						</Tooltip>
+					</Box>
+					<Box
+						sx={{
+							position: "flex",
+							alignContent: "center",
+							alignItems: "center",
+							border: "white 1px",
+							overflow: "hidden",
+						}}
+					>
+						{view === "calender" ? <MiniCalendar /> : <MeetingsList />}
+					</Box>
+				</Box>
 
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            border: "1px solid #white",
-            borderRadius: "8px",
-            justifyContent: "space-between",
-            height: "100%",
-          }}
-        >
-          <Box sx={{ display: "flex", justifyContent: "space-between", gap: 2, ml: 2, mt: 2, mr: 2 }}>
-            <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-              <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: 1 }}>
-                List of posts
-              </Typography>
-              <Typography variant="body2" sx={{ color: "#555", marginBottom: 1.3, ml: 1 }}>
-                Click on a post to view details
-              </Typography>
-            </Box>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						border: "1px solid #white",
+						borderRadius: "8px",
+						justifyContent: "space-between",
+						height: "100%",
+					}}
+				>
+					<Box sx={{ display: "flex", justifyContent: "space-between", gap: 2, ml: 2, mt: 2, mr: 2 }}>
+						<Box sx={{ display: "flex", alignItems: "flex-end" }}>
+							<Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: 1 }}>
+								List of posts
+							</Typography>
+							<Typography variant="body2" sx={{ color: "#555", marginBottom: 1.3, ml: 1 }}>
+								Click on a post to view details
+							</Typography>
+						</Box>
 
-            <Tooltip
-              title="Posts are visible to tutors. Tutors can request to help you, in which case you can accept their help. You can create a new post, or edit and delete an existing post."
-              arrow
-            >
-              <IconButton
-                sx={{
-                  alignItems: "right",
-                }}
-                aria-label="info"
-              >
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-          </Box>
-          <Box sx={{ display: "flex", gap: 2 }}>
-            <MiniPostList posts={posts} isLoading={postsLoading} isError={postsError} />
-          </Box>
-          <Box sx={{ display: "flex", gap: 2, mb: 2, mr: 2, justifyContent: "end" }}>
-            <CustomButton onClick={() => setShowCollabDialog(true)} variant="contained" color="primary" sx={{ fontSize: "18px" }}>
-              View all
-            </CustomButton>
-            <CustomButton variant="contained" color="primary" sx={{ fontSize: "18px" }}>
-              Create a post
-            </CustomButton>
-          </Box>
-        </Box>
+						<Tooltip
+							title="Posts are visible to tutors. Tutors can request to help you, in which case you can accept their help. You can create a new post, or edit and delete an existing post."
+							arrow
+						>
+							<IconButton
+								sx={{
+									alignItems: "right",
+								}}
+								aria-label="info"
+							>
+								<InfoIcon />
+							</IconButton>
+						</Tooltip>
+					</Box>
+					<Box sx={{ display: "flex", gap: 2 }}>
+						<MiniPostList posts={posts} isLoading={postsLoading} isError={postsError} />
+					</Box>
+					<Box sx={{ display: "flex", gap: 2, mb: 2, mr: 2, justifyContent: "end" }}>
+						<CustomButton
+							onClick={() => setShowCollabDialog(true)}
+							variant="contained"
+							color="primary"
+							sx={{ fontSize: "18px" }}
+						>
+							View all
+						</CustomButton>
+						<CustomButton variant="contained" color="primary" sx={{ fontSize: "18px" }}>
+							Create a post
+						</CustomButton>
+					</Box>
+				</Box>
 
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            border: "1px solid #white",
-            borderRadius: "8px",
-            justifyContent: "space-between",
-            height: "100%",
-          }}
-        >
-          <Box sx={{ display: "flex", justifyContent: "space-between", gap: 2, ml: 2, mt: 2, mr: 2 }}>
-            <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-              <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: 1 }}>
-                Your Active Collaborations
-              </Typography>
-              <Typography variant="body2" sx={{ color: "#555", marginBottom: 1.3, ml: 1 }}>
-                Click to view collaboration page
-              </Typography>
-            </Box>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						border: "1px solid #white",
+						borderRadius: "8px",
+						justifyContent: "space-between",
+						height: "100%",
+					}}
+				>
+					<Box sx={{ display: "flex", justifyContent: "space-between", gap: 2, ml: 2, mt: 2, mr: 2 }}>
+						<Box sx={{ display: "flex", alignItems: "flex-end" }}>
+							<Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: 1 }}>
+								Your Active Collaborations
+							</Typography>
+							<Typography variant="body2" sx={{ color: "#555", marginBottom: 1.3, ml: 1 }}>
+								Click to view collaboration page
+							</Typography>
+						</Box>
 
-            <Tooltip
-              title="Each collaboration with a tutor has a specific collaboration page. To view this page, simply click on one of the collaborations. When creating a collaboration, you can either find a tutor and request a collaboration with them, create a post which tutors can see, or request help from the administrator to find a fitting tutor."
-              arrow
-            >
-              <IconButton
-                sx={{
-                  alignItems: "right",
-                }}
-                aria-label="info"
-              >
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-          </Box>
-          <Box sx={{ display: "flex", gap: 2 }}>
-            <MiniCollabList collaborations={collaborations} isLoading={collabLoading} isError={collabError} />
-          </Box>
-          <Box sx={{ display: "flex", gap: 2, mb: 2, mr: 2, justifyContent: "end" }}>
-            <CustomButton variant="contained" color="primary" sx={{ fontSize: "18px" }}>
-              View more
-            </CustomButton>
-          </Box>
-        </Box>
-      </MediumShortOnShortBoxLayout>
-    </ThemeProvider>
-  );
+						<Tooltip
+							title="Each collaboration with a tutor has a specific collaboration page. To view this page, simply click on one of the collaborations. When creating a collaboration, you can either find a tutor and request a collaboration with them, create a post which tutors can see, or request help from the administrator to find a fitting tutor."
+							arrow
+						>
+							<IconButton
+								sx={{
+									alignItems: "right",
+								}}
+								aria-label="info"
+							>
+								<InfoIcon />
+							</IconButton>
+						</Tooltip>
+					</Box>
+					<Box sx={{ display: "flex", gap: 2 }}>
+						<MiniCollabList collaborations={collaborations} isLoading={collabLoading} isError={collabError} />
+					</Box>
+					<Box sx={{ display: "flex", gap: 2, mb: 2, mr: 2, justifyContent: "end" }}>
+						<CustomButton variant="contained" color="primary" sx={{ fontSize: "18px" }}>
+							View more
+						</CustomButton>
+					</Box>
+				</Box>
+			</MediumShortOnShortBoxLayout>
+		</ThemeProvider>
+	);
 }
