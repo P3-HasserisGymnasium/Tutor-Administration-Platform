3f35bfc75e21b73994ce46266743d6eb076837b6
Lynneruup <andreaslynnerup@outlook.dk>
2024-12-10 20:38:12 +0100

    merging resolving

--- frontend/src/components/content_components/PostListComponents/PostFilter.tsx
+++ frontend/src/components/content_components/PostListComponents/PostFilter.tsx
@@ -2,7 +2,6 @@ import { Box, Stack, Typography, Button } from "@mui/material";
 import SetDuration from "../SetDuration";
 import { FormProvider, useForm, useWatch } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { zodPostListFilterSchema } from "../../../types/data_types";
 import { PostListFilterType, zodPostListFilterSchema } from "../../../types/data_types";
 import CustomAutocomplete from "../CustomAutocomplete";
 
@@ -20,21 +19,6 @@ export default function PostFilter() {
     console.log(values);
   };
 
-  return (
-    <FormProvider {...filterMethods}>
-      <Stack spacing={1.5} sx={{ padding: "1em", height: "95%" }}>
-        <Typography variant="h2">Filters</Typography>
-        <CustomAutocomplete variant="subject" multiple={true} />
-        <SetDuration />
-        <Box sx={{ flexGrow: 1 }} />
-        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <Button sx={{ width: "100%" }} onClick={handleSubmit(filter)}>
-            Filter
-          </Button>
-        </Box>
-      </Stack>
-    </FormProvider>
-  );
   return (
     <FormProvider {...filterMethods}>
       <Stack spacing={1.5} sx={{ padding: "1em", height: "95%" }}>
