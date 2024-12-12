b0808d36da92c7a27c15fdb103152eedb414d424
Li-Thao-Phung <lphung23@student.aau.dk>
2024-12-12 10:32:26 +0100

    .

--- frontend/src/components/page_components/tutee/TuteeProfilePage.tsx
+++ frontend/src/components/page_components/tutee/TuteeProfilePage.tsx
@@ -1,22 +1,45 @@
+import { Box, Paper} from "@mui/material";
 import DeleteAccountDialog from "../dialogs/DeleteAccountDialog";
 import { useState } from "react";
-import { Box } from "@mui/material";
+//import { useAuth } from "~/api/authentication/useAuth";
+
+import {useWrap, useVariableWidth, useVariableHeight } from "~/utilities/helperFunctions";
 
 export default function TuteeProfilePage() {
   const [isDeleteAcountDialogOpen, setIsDeleteAcountDialogOpen] = useState(false);
+
+  //const {userState} = useAuth();
+
+
   return (
 
-    <Box sx={{
-      display: 'flex',
-      minHeight: '100vh',
-      boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-      maxWidth: "800px",
-      
 
-    }}>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexWrap: useWrap(),
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <Paper elevation={3}
+      sx={{
+        margin: 1,
+        width: useVariableWidth(1),
+        height: useVariableHeight("calc(100% - 16px)"),
+      }}>
+        
+      </Paper>
+
       <DeleteAccountDialog open={isDeleteAcountDialogOpen} setOpen={setIsDeleteAcountDialogOpen} />
-      content
     </Box>
+
+   
+
+
 
  
   );
