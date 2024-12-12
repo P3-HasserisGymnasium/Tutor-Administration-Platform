9c74a8eb1dceffa746ad62ed8ed0f2cb540d437d
Lynneruup <andreaslynnerup@outlook.dk>
2024-12-12 17:45:01 +0100

    layout fix and open dialog button fix

--- frontend/src/components/content_components/PostListComponents/PostList.tsx
+++ frontend/src/components/content_components/PostListComponents/PostList.tsx
@@ -4,14 +4,13 @@ import { SubjectType } from "~/types/data_types";
 import { usePostService } from "~/api/services/post-service";
 import { CircularProgress } from "@mui/material";
 
-export default function PostList({filters, loading}:{filters:{duration: number[]; subjects: SubjectType[];}, loading:boolean}) {
-  const {data: posts, isError} = usePostService().useGetPosts(filters);
-
+export default function PostList({ filters, loading }: { filters: { duration: number[]; subjects: SubjectType[] }; loading: boolean }) {
+  const { data: posts, isError } = usePostService().useGetPosts(filters);
 
   if (loading) {
     return (
       <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: 4, width: "100%", height: "90%" }}>
-        <CircularProgress  size={100}/>
+        <CircularProgress size={100} />
       </Box>
     );
   }
@@ -22,29 +21,34 @@ export default function PostList({filters, loading}:{filters:{duration: number[]
         Error fetching posts. Please refresh the page.
       </Typography>
     );
-  }
-  else return(
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "1em",
-        margin: "1em",
-      }}
-    >
-      <Typography variant="h2">Filtered Posts</Typography>
-
-      {posts?.length === 0 && <Typography variant="h2">No posts found</Typography>}
-
-      <Stack
-        spacing={1}
+  } else
+    return (
+      <Box
         sx={{
-          overflowY: "auto",
-          marginTop: "0.5em",
+          display: "flex",
+          flexDirection: "column",
+          gap: "1em",
+          margin: "1em",
+          height: "100%",
         }}
       >
-        {posts?.map((post) => {return <PostCard key={post.id} post={post} />})}
-      </Stack>
-    </Box>
-  )
+        <Typography variant="h2">Filtered Posts</Typography>
+
+        {posts?.length === 0 && <Typography variant="h2">No posts found</Typography>}
+
+        <Stack
+          spacing={1}
+          sx={{
+            maxHeight: "100%",
+            height: "90%",
+            overflowY: "scroll",
+            marginTop: "0.5em",
+          }}
+        >
+          {posts?.map((post) => {
+            return <PostCard key={post.id} post={post} />;
+          })}
+        </Stack>
+      </Box>
+    );
 }
