b64ddc3ffdbb6b3ec6a344b0128832c48c533bc1
Patrick <patrick@reiffenste.in>
2024-12-13 16:20:24 +0100

    fsf

--- frontend/src/components/page_components/admin/admin_components/CollaborationBox.tsx
+++ frontend/src/components/page_components/admin/admin_components/CollaborationBox.tsx
@@ -1,14 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import { CollaborationType } from "~/types/entity_types";
+import AdminRequestDialog from "../../dialogs/AdminRequestsDialog";
+import { useState } from "react";
 
 type CollaborationBoxProps = {
     collaboration: CollaborationType
 }
 
 export default function CollaborationBox({ collaboration }: CollaborationBoxProps) {
+    const [showCollaborationDialog, setShowCollaborationDialog] = useState(false);
 
     return (
-        <Box sx={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
+        <Box onClick={() => { setShowCollaborationDialog(true) }} sx={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
+            <AdminRequestDialog open={showCollaborationDialog} setOpen={setShowCollaborationDialog} request={collaboration} />
             <Typography>
                 paring?: {collaboration.requestedPairing == true ? "yay" : "nay"}<br></br>
                 id: {collaboration.id}
