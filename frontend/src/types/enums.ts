6c0b3d8bc0d27db445202d2b06e73bdb6f33f140
Kasia-eto <143899969+Kasia-eto@users.noreply.github.com>
2024-12-13 11:36:39 +0100

    enum farver - blevet mørker

--- frontend/src/types/enums.ts
+++ frontend/src/types/enums.ts
@@ -70,21 +70,22 @@ export type TimeAvailability = {
 
 // Mapping colors to subject
 export const SubjectColors: Record<Subject, subjectStyling> = {
-	[Subject.MUSIC]: { background: "#b2c9e0", color: "white" },
-	[Subject.MATH]: { background: "#ffcccb", color: "white" },
-	[Subject.ESS]: { background: "#c1e1c5", color: "white" },
-	[Subject.ENGLISH]: { background: "#ffd700", color: "white" },
-	[Subject.DANISH]: { background: "#FF4141", color: "white" },
-	[Subject.GERMAN]: { background: "#87EB8E", color: "white" },
-	[Subject.FRENCH]: { background: "#1B76FF", color: "white" },
-	[Subject.BIOLOGY]: { background: "#98fb98", color: "white" },
-	[Subject.CHEMISTRY]: { background: "#dcdcdc", color: "white" },
-	[Subject.PE]: { background: "#ffa07a", color: "white" },
-	[Subject.VA]: { background: "#ffdead", color: "white" },
-	[Subject.PSYCHOLOGY]: { background: "#dda0dd", color: "white" },
-	[Subject.ECON]: { background: "#add8e6", color: "white" },
-	[Subject.SS]: { background: "#f0e68c", color: "white" },
+	[Subject.MUSIC]: { background: "#7fa6c8", color: "white" },
+	[Subject.MATH]: { background: "#d96f6f", color: "white" },
+	[Subject.ESS]: { background: "#87b890", color: "white" },
+	[Subject.ENGLISH]: { background: "#d1b000", color: "white" },
+	[Subject.DANISH]: { background: "#c23c3c", color: "white" },
+	[Subject.GERMAN]: { background: "#55b257", color: "white" },
+	[Subject.FRENCH]: { background: "#1c5ebd", color: "white" },
+	[Subject.BIOLOGY]: { background: "#6bb859", color: "white" },
+	[Subject.CHEMISTRY]: { background: "#bfbfbf", color: "white" },
+	[Subject.PE]: { background: "#d47b6c", color: "white" },
+	[Subject.VA]: { background: "#d4b287", color: "white" },
+	[Subject.PSYCHOLOGY]: { background: "#bc7bbc", color: "white" },
+	[Subject.ECON]: { background: "#84a3c6", color: "white" },
+	[Subject.SS]: { background: "#c5b053", color: "white" },
 };
+
 
 // interface for subject styling
 interface subjectStyling {
