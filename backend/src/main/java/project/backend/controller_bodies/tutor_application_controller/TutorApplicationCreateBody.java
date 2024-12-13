412b072344ea8d453f6a5b3e88722e3bfb921e33
Lynneruup <andreaslynnerup@outlook.dk>
2024-12-13 11:09:25 +0100

    TutorApplication implemented, including notification

--- backend/src/main/java/project/backend/controller_bodies/tutor_application_controller/TutorApplicationCreateBody.java
+++ backend/src/main/java/project/backend/controller_bodies/tutor_application_controller/TutorApplicationCreateBody.java
@@ -6,8 +6,7 @@ import project.backend.model.SubjectEnum;
 
 public class TutorApplicationCreateBody {
     
-    public Long student_id;
-
+    public Long user_id;
     public List<SubjectEnum> subjects;
     public String tutor_profile_description;
     public List<TutorTimeSlotCreateBody> time_availability;
